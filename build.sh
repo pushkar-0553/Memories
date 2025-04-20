a1e6f768428766a2576545aee16ca079dd49e17e
pushkar-0553 <105909645+pushkar-0553@users.noreply.github.com>
2025-04-21 00:22:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('vema', 'vemalathay@gmail.com', 'your-password-here') if not User.objects.filter(username='vema').exists() else None" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('vema', 'vemalathay@gmail.com', 'Vema@123') if not User.objects.filter(username='vema').exists() else None" | python manage.py shell
