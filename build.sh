2293f41f810f8d235a8a812205a96c68fd6f9534
Temporary User <temp@example.com>
2025-04-21 00:16:55 +0530

    Update build.sh to create superuser during deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('vema', 'vemalathay@gmail.com', 'your-password-here') if not User.objects.filter(username='vema').exists() else None" | python manage.py shell
