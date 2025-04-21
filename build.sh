cf4b30b22b1a5a128d6ca9101e15bb75435ec9a0
Temporary User <temp@example.com>
2025-04-21 07:52:38 +0530

    Update build script with direct superuser creation

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_superuser
+
+# Create superuser
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Admin@123') if not User.objects.filter(username='admin').exists() else print('Superuser already exists')" | python manage.py shell
+
 python manage.py import_students
